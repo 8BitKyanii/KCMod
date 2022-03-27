109125df43bf675a6eb3641b2a345c9e41a443d4
8-Bit Kyanii <79796142+8BitKyanii@users.noreply.github.com>
2022-03-27 12:47:46 -0400

    Add project files.

---
+++ Items/Accessories/RingOfLife.cs
@@ -0,0 +1,37 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace KCMod.Items.Accessories
+{
+    public class RingOfLife : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("'Become Unstoppable Today!'");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 16;
+            item.height = 28;
+            item.value = Item.sellPrice(platinum: 20);
+            item.rare = ItemRarityID.Yellow;
+            item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.statLifeMax2 += 300;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(499, 4);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
